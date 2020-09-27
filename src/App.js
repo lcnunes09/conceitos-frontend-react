34027366527696559e09443c0f07c991cad8a748
Luciana Nunes <lcnunes09@gmail.com>
2020-09-27 20:38:28 -0300

    Add connection frontend and backend; Add list project and new project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from './services/api';
 
 import './App.css';
 
 import Header from './components/Header'
 
 function App() {
-    const [ projects, setProjects ] = useState(['Desenvolvimento Web', 'Desenvolvimento Mobile']);
+    const [ projects, setProjects ] = useState([]);
 
-    function handleAddProject() {
-        // projects.push(`Novo Projeto ${Date.now()}`);
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data)
+        })
+    }, []);
 
-        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    async function handleAddProject() {
+        const response = await api.post('projects', {
+            title: `Mobile com React Native  ${Date.now()}`,
+            owner: 'Luciana Nunes'
+        });
+        
+        const project = response.data;
 
-        console.log(projects);
+        setProjects([...projects, project]);
     }
 
     return (
@@ -20,7 +31,7 @@ function App() {
             <Header title="Projects" />
 
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
