dcb3f12c4ef6cb7bc5ae86690eb587cea5410ed4
Luciana Nunes <lcnunes09@gmail.com>
2020-09-27 20:08:00 -0300

    Remove background image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg'
 
 import Header from './components/Header'
 
@@ -19,8 +18,6 @@ function App() {
     return (
         <>
             <Header title="Projects" />
-            
-            <img width={300} src={backgroundImage} alt="Imagem de background"/>
 
             <ul>
                 {projects.map(project => <li key={project}>{project}</li>)}
